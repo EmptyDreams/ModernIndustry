55af6356799d4ef5e53037d10ef35e8578a2f067
luqin2007 <lq2007lq@hotmail.com>
2021-02-16 10:18:38 +0800

    int

--- src/main/java/xyz/emptydreams/mi/api/gui/common/GuiLoader.java
+++ src/main/java/xyz/emptydreams/mi/api/gui/common/GuiLoader.java
@@ -1,14 +1,13 @@
 package xyz.emptydreams.mi.api.gui.common;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import xyz.emptydreams.mi.ModernIndustry;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * GUI的总加载器
@@ -17,22 +16,23 @@ import java.util.Map;
 public class GuiLoader implements IGuiHandler {
 	
 	/** 存储每个ID及其对应的构建器 */
-	private final static Map<Int, IContainerCreater> IDS = new LinkedHashMap<>();
+	private final static Int2ObjectMap<IContainerCreater> IDS = new Int2ObjectOpenHashMap<>();
 	
 	public GuiLoader() {
 		NetworkRegistry.INSTANCE.registerGuiHandler(ModernIndustry.instance, this);
+		IDS.defaultReturnValue(null);
 	}
 	
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		IContainerCreater creater = IDS.getOrDefault(new Int(ID), null);
-		return creater == null ? null : creater.createService(world, player, new BlockPos(x, y, z));
+		IContainerCreater creator = IDS.get(ID);
+		return creator == null ? null : creator.createService(world, player, new BlockPos(x, y, z));
 	}
 	
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		IContainerCreater creater = IDS.getOrDefault(new Int(ID), null);
-		return creater == null ? null : creater.createClient(world, player, new BlockPos(x, y, z));
+		IContainerCreater creator = IDS.get(ID);
+		return creator == null ? null : creator.createClient(world, player, new BlockPos(x, y, z));
 	}
 	
 	/** 存储ID自动分配位点 */
@@ -42,10 +42,10 @@ public class GuiLoader implements IGuiHandler {
 	 * 创建一个ID，由系统自动分配
 	 * @return 如果ID分配已经到达数量上限则返回-1
 	 */
-	public static int register(IContainerCreater creater) {
+	public static int register(IContainerCreater creator) {
 		int i;
 		do {
-			i = register(++IDown, creater);
+			i = register(++IDown, creator);
 		} while (i == -1 && IDown != Integer.MAX_VALUE);
 		return i;
 	}
@@ -53,42 +53,14 @@ public class GuiLoader implements IGuiHandler {
 	/**
 	 * 创建指定ID
 	 * @param ID 指定ID
-	 * @param creater 构建器
+	 * @param creator 构建器
 	 * @return 当ID无法创建时返回-1
 	 */
-	private static int register(int ID, IContainerCreater creater) {
-		Int id = new Int(ID);
-		if (!IDS.containsKey(id)) {
-			IDS.put(id, creater);
+	private static int register(int ID, IContainerCreater creator) {
+		if (!IDS.containsKey(ID)) {
+			IDS.put(ID, creator);
 			return ID;
 		}
 		else return -1;
 	}
-	
-	private static final class Int implements Comparable<Int> {
-		public final int i;
-		
-		public Int(int i) {
-			this.i = i;
-		}
-		
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (o == null || getClass() != o.getClass()) return false;
-			Int anInt = (Int) o;
-			return i == anInt.i;
-		}
-		
-		@Override
-		public int hashCode() {
-			return i;
-		}
-		
-		@Override
-		public int compareTo(GuiLoader.Int o) {
-			return Integer.compare(i, o.i);
-		}
-	}
-	
 }
