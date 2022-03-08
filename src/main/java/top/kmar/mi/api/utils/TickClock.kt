2d72d001774b3e700e11415fcc47b4e668841289
EmptyDreams <minedreams@qq.com>
2022-03-08 17:24:15 +0800

    添加`TickClock`

---
+++ src/main/java/top/kmar/mi/api/utils/TickClock.kt
@@ -0,0 +1,21 @@
+package top.kmar.mi.api.utils
+
+/**
+ * tick任务
+ * @param cycle 任务周期
+ * @author EmptyDreams
+ */
+class TickClock(val cycle: Int) {
+
+    private var counter = 0
+
+    /** 判断是否不需要执行任务并让计数器加一 */
+    fun notContinue(): Boolean {
+        if (++counter == cycle) counter = 0
+        return counter != 0
+    }
+
+    /** 判断是否需要执行任务并让计数器加一 */
+    fun isContinue() = !notContinue()
+
+}
