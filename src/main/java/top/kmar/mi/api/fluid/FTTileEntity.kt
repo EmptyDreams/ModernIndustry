adae71ff22814c0fec8e6abb33e1976781a61127
EmptyDreams <minedreams@qq.com>
2022-03-12 20:50:26 +0800

    修复管道向外界送水时不检测方向上是否有开口的问题

--- src/main/java/top/kmar/mi/api/fluid/FTTileEntity.kt
+++ src/main/java/top/kmar/mi/api/fluid/FTTileEntity.kt
@@ -126,7 +126,7 @@ abstract class FTTileEntity : BaseTileEntity(), IAutoNetwork, IFluid, ITickable 
             if (!simulate) fluidData = newData
             for (value in PUSH_EACH_PRIORITY) {
                 if (queue.isEmpty) break
-                if (value == facing.opposite) continue
+                if (value == facing.opposite || !isOpen(value)) continue
                 val fluid = getFluidDirect(value)
                 result += fluid?.insert(queue, value, simulate, report) ?:
                                 pump2World(queue, facing, simulate, report)
