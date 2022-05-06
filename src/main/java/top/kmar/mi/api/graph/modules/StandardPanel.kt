15307a87db7ca89fddc848246d992d0d7b2937d5
EmptyDreams <minedreams@qq.com>
2022-05-06 17:40:34 +0800

    添加字符串控件

--- src/main/java/top/kmar/mi/api/graph/modules/StandardPanel.kt
+++ src/main/java/top/kmar/mi/api/graph/modules/StandardPanel.kt
@@ -5,7 +5,7 @@ import top.kmar.mi.api.graph.utils.GeneralPanel
 import top.kmar.mi.api.graph.utils.PanelBuilder
 import top.kmar.mi.api.utils.WorldUtil
 
-typealias Builder = PanelBuilder
+private typealias Builder = PanelBuilder
 
 object StandardPanel {
 
@@ -14,6 +14,9 @@ object StandardPanel {
         if (WorldUtil.isServer()) GeneralPanel()
         else BackgroundPanelClient(builder.x, builder.y, builder.width, builder.height)
 
-
+    @JvmStatic
+    fun buildString(builder: Builder): IPanel =
+        if (WorldUtil.isServer()) GeneralPanel()
+        else StringPanelClient(builder.x, builder.y, builder.width, builder.height)
 
 }
