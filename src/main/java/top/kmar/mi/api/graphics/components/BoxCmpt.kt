71def53d18111d8ecbdffd072ce9315197b6381d
EmptyDreams <minedreams@qq.com>
2022-10-09 20:20:10 +0800

    添加辅助排版的控件`BoxCmpt`

---
+++ src/main/java/top/kmar/mi/api/graphics/components/BoxCmpt.kt
@@ -0,0 +1,29 @@
+package top.kmar.mi.api.graphics.components
+
+import top.kmar.mi.api.graphics.components.interfaces.Cmpt
+import top.kmar.mi.api.graphics.components.interfaces.CmptAttributes
+import top.kmar.mi.api.graphics.components.interfaces.CmptClient
+import top.kmar.mi.api.graphics.utils.GraphicsStyle
+import top.kmar.mi.api.graphics.utils.InheritSizeMode
+import top.kmar.mi.api.register.others.AutoCmpt
+
+/**
+ * 空的控件
+ * @author EmptyDreams
+ */
+@AutoCmpt("div")
+class BoxCmpt(attributes: CmptAttributes) : Cmpt(attributes) {
+
+    override fun initClientObj() = BoxCmptClient()
+
+    inner class BoxCmptClient : CmptClient {
+
+        override val service = this@BoxCmpt
+        override val style = GraphicsStyle(service).apply {
+            width = InheritSizeMode { parent.client.style.width() }
+            height = InheritSizeMode { parent.client.style.height() }
+        }
+
+    }
+
+}
