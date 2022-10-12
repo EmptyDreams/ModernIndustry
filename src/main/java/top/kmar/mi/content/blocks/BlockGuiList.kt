5f8ac9a6a781e54d981636e78c772ab096f81094
EmptyDreams <minedreams@qq.com>
2022-10-12 14:55:34 +0800

    将测试GUI改为水泵的GUI

---
+++ src/main/java/top/kmar/mi/content/blocks/BlockGuiList.kt
@@ -0,0 +1,10 @@
+package top.kmar.mi.content.blocks
+
+import net.minecraft.util.ResourceLocation
+import top.kmar.mi.ModernIndustry.MODID
+
+object BlockGuiList {
+
+    val fluidPump = ResourceLocation(MODID, "fluidPump")
+
+}
--- src/main/java/top/kmar/mi/content/blocks/machine/user/FluidPumpBlock.kt
+++ src/main/java/top/kmar/mi/content/blocks/machine/user/FluidPumpBlock.kt
@@ -10,16 +10,15 @@ import net.minecraft.item.ItemBlock
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.EnumHand
-import net.minecraft.util.ResourceLocation
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
-import top.kmar.mi.ModernIndustry
 import top.kmar.mi.api.capabilities.fluid.FluidCapability
 import top.kmar.mi.api.capabilities.fluid.IFluid
 import top.kmar.mi.api.register.block.annotations.AutoBlockRegister
 import top.kmar.mi.api.utils.getPlacingDirection
 import top.kmar.mi.api.utils.whatFacing
+import top.kmar.mi.content.blocks.BlockGuiList
 import top.kmar.mi.content.blocks.CommonUtil
 import top.kmar.mi.content.blocks.base.MachineBlock
 import top.kmar.mi.content.items.base.ItemBlockExpand
@@ -122,7 +121,7 @@ open class FluidPumpBlock : MachineBlock(Material.IRON) {
         worldIn: World, pos: BlockPos, state: IBlockState,
         playerIn: EntityPlayer, hand: EnumHand, facing: EnumFacing,
         hitX: Float, hitY: Float, hitZ: Float
-    ) = CommonUtil.openGui(playerIn, ResourceLocation(ModernIndustry.MODID, "test"), pos)
+    ) = CommonUtil.openGui(playerIn, BlockGuiList.fluidPump, pos)
 
     private val item: ItemBlock by lazy(PUBLICATION) { ItemBlockExpand(this) }
 
