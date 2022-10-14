18081935cc69aaa3ff7e01c3a5df2c045386373e
EmptyDreams <minedreams@qq.com>
2022-10-14 21:25:33 +0800

    控件尺寸计算支持`inherit`直接继承父元素尺寸

--- src/main/java/top/kmar/mi/api/graphics/parser/cache/SizeParserCache.kt
+++ src/main/java/top/kmar/mi/api/graphics/parser/cache/SizeParserCache.kt
@@ -2,10 +2,7 @@ package top.kmar.mi.api.graphics.parser.cache
 
 import net.minecraftforge.fml.relauncher.Side
 import net.minecraftforge.fml.relauncher.SideOnly
-import top.kmar.mi.api.graphics.utils.FixedSizeMode
-import top.kmar.mi.api.graphics.utils.GraphicsStyle
-import top.kmar.mi.api.graphics.utils.ISizeMode
-import top.kmar.mi.api.graphics.utils.PercentSizeMode
+import top.kmar.mi.api.graphics.utils.*
 import top.kmar.mi.api.utils.removeAllSpace
 
 /**
@@ -25,7 +22,9 @@ class SizeParserCache(exp: String, isHeight: Boolean) : IParserCache {
     private val task: ((GraphicsStyle) -> Int) -> ISizeMode = exp.removeAllSpace().run {
         // 通过exp解析出一个生成ISideMode的函数
         // 通过此举避免每次生成ISizeMode时都重新解析一遍字符串
-        if (endsWith('%')) {  // 如果字符串格式为“num%”
+        if (startsWith('i', true)) {
+            return@run { InheritSizeMode { style -> it(style) } }
+        } else if (endsWith('%')) {  // 如果字符串格式为“num%”
             val percent = substring(0 until length - 1).toDouble() * 0.01
             return@run { PercentSizeMode(percent, 0) { style -> it(style) } }
         } else if (this[0].isDigit()) {    // 如果字符串格式为“num”
