6f287d08a33c96949eb72d2118b91b7902cb10c2
EmptyDreams <minedreams@qq.com>
2022-10-24 12:01:06 +0800

    恢复调试工具的功能

--- src/main/java/top/kmar/mi/content/items/debug/ClassInfoViewer.java
+++ src/main/java/top/kmar/mi/content/items/debug/ClassInfoViewer.java
@@ -2,6 +2,7 @@ package top.kmar.mi.content.items.debug;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -9,28 +10,51 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import top.kmar.mi.ModernIndustry;
 import top.kmar.mi.api.register.item.AutoItemRegister;
+import top.kmar.mi.api.utils.MISysInfo;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 /**
+ * 调试工具
  * @author EmptyDreams
  */
 @AutoItemRegister("class_info_viewer")
 public class ClassInfoViewer extends Item {
-	
-	public ClassInfoViewer() {
-		setCreativeTab(ModernIndustry.TAB_DEBUG);
-	}
-	
-	@Override
-	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos,
-	                                  EnumHand hand, EnumFacing facing,
-	                                  float hitX, float hitY, float hitZ) {
-		return EnumActionResult.FAIL;
-		//TODO
-		/*TileEntity entity = worldIn.getTileEntity(pos);
-		if (entity == null) return EnumActionResult.PASS;
-		boolean result = CommonUtil.openGui(player, ClassInfoViewerFrame.NAME, worldIn, pos);
-		if (result) return EnumActionResult.SUCCESS;
-		return EnumActionResult.FAIL;*/
-	}
-	
+    
+    public ClassInfoViewer() {
+        setCreativeTab(ModernIndustry.TAB_DEBUG);
+    }
+    
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos,
+                                      EnumHand hand, EnumFacing facing,
+                                      float hitX, float hitY, float hitZ) {
+        TileEntity target = worldIn.getTileEntity(pos);
+        if (target == null) return EnumActionResult.FAIL;
+        StringBuilder sb = new StringBuilder();
+        if (worldIn.isRemote) sb.append("---------- Client ----------\n");
+        else sb.append("---------- Service ----------\n");
+        Class<?> clazz = target.getClass();
+        try {
+            while (clazz != Object.class) {
+                sb.append(clazz.getName()).append('\n');
+                for (Field field : clazz.getDeclaredFields()) {
+                    if (!Modifier.isPublic(field.getModifiers())) field.setAccessible(true);
+                    sb.append('\t')
+                            .append(field.getName())
+                            .append(":\t")
+                            .append(field.get(target))
+                            .append('\n');
+                }
+                clazz = clazz.getSuperclass();
+            }
+        } catch (Exception e) {
+            MISysInfo.err("打印信息时遇到意料之外的错误", e);
+            return EnumActionResult.FAIL;
+        }
+        MISysInfo.print(sb);
+        return EnumActionResult.SUCCESS;
+    }
+    
 }
